99a6252fe4c07b231a5241b58d51b49483107f51
HashBrownui <hashbrownui@gmail.com>
2025-12-03 23:21:26 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function formatElapsedTime() 
 {
   const currentTime = new Date();
-  const lastStreamEndTime = new Date("2025-12-3T03:30:30Z");
+  const lastStreamEndTime = new Date("2025-12-2T03:30:30Z");
   // in milliseconds
   const timeSinceStream = currentTime.getTime() - lastStreamEndTime.getTime();
   
@@ -75,6 +75,7 @@ function buildRandomNerissaStreamLink()
 
 //builds the first link upon page load
 buildRandomNerissaStreamLink();*/
+
 
 
 
