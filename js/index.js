a65abbd7c352d49ab4808edacafbb322ca20a533
HashBrownui <hashbrownui@gmail.com>
2026-02-07 16:49:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ setInterval(updateTimeSinceNerissaElement, 1000);
 function formatElapsedKaraTime() 
 {
   const currentKaraTime = new Date();
-  const lastKaraEndTime = new Date("2025-01-31T22:30:30Z");
+  const lastKaraEndTime = new Date("2026-01-31T22:30:30Z");
   // in milliseconds
   const timeSinceKara = currentKaraTime.getTime() - lastKaraEndTime.getTime();
   
@@ -75,6 +75,7 @@ function buildRandomNerissaStreamLink()
 
 //builds the first link upon page load
 buildRandomNerissaStreamLink();*/
+
 
 
 
