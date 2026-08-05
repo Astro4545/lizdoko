5ce4a5134436223a2e9c741ae25d9fccf5f925b7
HashBrownui <hashbrownui@gmail.com>
2026-08-05 22:38:28 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function formatElapsedTime() 
 {
   const currentTime = new Date();
-  const lastStreamEndTime = new Date("2026-07-19T10:00:30Z");
+  const lastStreamEndTime = new Date("2026-08-03T08:00:30Z");
   // in milliseconds
   const timeSinceStream = currentTime.getTime() - lastStreamEndTime.getTime();
   
